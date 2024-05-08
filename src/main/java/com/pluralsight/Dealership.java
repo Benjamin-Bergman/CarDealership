61885613c28cca87eda56f68b77d554f10b727f2
Benjamin-Bergman <git@gdane.net>
2024-05-08 09:47:53 -0400

    Add Filters

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -5,6 +5,7 @@
 package com.pluralsight;
 
 import java.util.*;
+import java.util.function.*;
 
 public class Dealership {
     private final String name;
@@ -48,5 +49,55 @@ public class Dealership {
 
     public void clear() {
         inventory.clear();
+    }
+
+    public static final class Filter {
+        private Filter() {
+            throw new InstantiationException("This class cannot be instantiated.");
+        }
+
+        public static Predicate<Vehicle> minPrice(double min) {
+            return v -> v.price() >= min;
+        }
+
+        public static Predicate<Vehicle> maxPrice(double max) {
+            return v -> v.price() <= max;
+        }
+
+        public static Predicate<Vehicle> make(String make) {
+            var cleaned = make.trim().toLowerCase();
+            return v -> v.make().toLowerCase().contains(cleaned);
+        }
+
+        public static Predicate<Vehicle> model(String model) {
+            var cleaned = model.trim().toLowerCase();
+            return v -> v.model().toLowerCase().contains(cleaned);
+        }
+
+        public static Predicate<Vehicle> minYear(int min) {
+            return v -> v.year() >= min;
+        }
+
+        public static Predicate<Vehicle> maxYear(int max) {
+            return v -> v.year() <= max;
+        }
+
+        public static Predicate<Vehicle> color(String color) {
+            var cleaned = color.trim().toLowerCase();
+            return v -> v.color().toLowerCase().contains(cleaned);
+        }
+
+        public static Predicate<Vehicle> minOdometer(int min) {
+            return v -> v.odometer() >= min;
+        }
+
+        public static Predicate<Vehicle> maxOdometer(int max) {
+            return v -> v.odometer() <= max;
+        }
+
+        public static Predicate<Vehicle> type(String type) {
+            var cleaned = type.trim().toLowerCase();
+            return v -> v.vehicleType().toLowerCase().contains(cleaned);
+        }
     }
 }
