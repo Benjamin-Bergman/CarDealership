fdca34fe168ba84fba656482e7c9a570e1f523d9
Benjamin-Bergman <git@gdane.net>
2024-05-17 14:16:13 -0400

    Add concrete Contract implementations

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -10,12 +10,24 @@ import manifold.ext.props.rt.api.*;
  * Represents a contract for a sold/leased {@link Vehicle}.
  */
 public interface Contract {
+    /**
+     * The total price of this contract.
+     */
     @val
     double totalPrice;
+    /**
+     * The monthly payment for this contract.
+     */
     @val
     double monthlyPayment;
+    /**
+     * The term lengths of this contract in months.
+     */
     @val
     int paymentLength;
+    /**
+     * The vehicle sold for this contract.
+     */
     @val
     Vehicle vehicleSold;
 }
